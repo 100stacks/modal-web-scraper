983133f0b21d10442dbf20f341bebe0fde374f33
100stacks <100stacks@users.noreply.github.com>
2024-06-08 19:07:30 -0500

    modal: add Playwright custom container to handle dynamic content

--- app/scrape.py
+++ app/scrape.py
@@ -6,14 +6,39 @@ import modal # host code on Modal serverless platform
 
 app = modal.App(name="weblink-scraper")
 
-@app.function()
-def get_links(url):
-    response = urllib.request.urlopen(url)
-    html = response.read().decode("utf-8")
-    links = []
+"""
+    Custom containers
 
-    for match in re.finditer('href="(.*?)"', html):
-        links.append(match.group(1))
+    Playwright - used to launch a headless Chromium browser.  Interprets any JS on a webpage.
+    modal.Image - pre-bundled image - `modal.Image.debian_slim`
+
+"""
+playwright_image = modal.Image.debian_slim(python_version="3.10").run_commands(
+    "apt-get update",
+    "apt-get install -y software-properties-common",
+    "apt-add-repository non-free",
+    "apt-add-repository contrib",
+    "pip install playwright==1.30.0",
+    "playwright install-deps chromium",
+    "playwright install chromium",
+)
+
+@app.function(image=playwright_image)
+async def get_links(cur_url: str):
+    from playwright.async_api import async_playwright
+
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+        page = await browser.new_page()
+
+        # scrape links on webpage
+        await page.goto(cur_url)
+        links = await page.eval_on_selector_all("a[href]", "elements => elements.map(element => element.href)")
+
+        # close session
+        await browser.close()
+
+    print("Links", links)
 
     return links
 
